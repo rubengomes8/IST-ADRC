4cd0c247e3cb49ba0fd3647cbb64ccc71a8c921d
rubengomes8 <you@example.com>
2019-09-20 20:33:13 +0100

    cabeçalhos das funcoes do enunciado

--- binaryTree.c
+++ binaryTree.c
@@ -1,5 +1,7 @@
 #include "binaryTree.h"
 
+//If left = right = null --> the node is a leaf
+
 struct _binaryTree{
 	int nextHop;
 	struct _binaryTree *left;
@@ -7,7 +9,7 @@ struct _binaryTree{
 };
 
 
-struct _binaryTree *newNode(int nextHop){
+binaryTree *newNode(int nextHop){
 
 	struct _binaryTree *node = (struct _binaryTree*) malloc(sizeof(struct _binaryTree));
 
@@ -17,3 +19,52 @@ struct _binaryTree *newNode(int nextHop){
 	node->right = NULL;
 	return node;
 }
+
+//Remove o nó. Nesta função só entra como argumentos folhas da árvore que se prentende remover. Se não for folha não se remove
+//No código que invoca esta função, o pai tem de meter ponteiro que apontava para este nó a NULL
+void removeNode(binaryTree *nodeToBeRemoved){
+	free(nodeToBeRemoved);
+}
+
+
+
+binaryTree *PrefixTree(FILE *fp){
+	binaryTree *root = NULL;
+	return root;
+}
+
+void PrintTable(binaryTree *root){
+
+}
+
+
+int LookUp(binaryTree *root, char *address){
+
+	return 0;
+}
+
+binaryTree *InsertPrefix(binaryTree *root, char *prefix, int nextHop){
+
+	return root;
+}
+
+
+binaryTree *DeletePrefix(binaryTree *root, char *prefix){
+
+	return root;
+}
+
+
+binaryTree *CompressTree(binaryTree *root){
+
+	return root;
+}
+/*
+PrefixTree, that reads a prefix table from a text file and returns a prefix tree representation of that table;
+• PrintTable, that receives as input a prefix tree and prints to screen the corresponding prefix table;
+• LookUp, that receives as input a prefix tree and an address and returns the next-hop for that address;
+• InsertPrefix, that receives as input a prefix tree, a prefix and the associated next-hop, and returns a prefix tree
+with the prefix included;
+• DeletePrefix, that receives as input a prefix tree and a prefix and returns a prefix tree with the prefix withdrawn;
+• CompressTree, that receives as input the prefix tree of a prefix table and returns another prefix tree representing
+a prefix table with a smaller number of entries.*/
--- binaryTree.h
+++ binaryTree.h
@@ -4,3 +4,15 @@
 #include <stdio.h>
 
 typedef struct _binaryTree binaryTree;
+
+
+binaryTree *newNode(int nextHop);
+void removeNode(binaryTree *nodeToBeRemoved);
+
+
+binaryTree *PrefixTree(FILE *fp);
+void PrintTable(binaryTree *root);
+int LookUp(binaryTree *root, char *address);
+binaryTree *InsertPrefix(binaryTree *root, char *prefix, int nextHop);
+binaryTree *DeletePrefix(binaryTree *root, char *prefix);
+binaryTree *CompressTree(binaryTree *root);
